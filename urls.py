8698b87524633d28f191c2e1aac8c04621e2d4df
Matthew Riley MacPherson <mmacpherson@mozilla.com>
2011-12-29 15:41:05 -0400

    Add basic error pages and url handlers

--- urls.py
+++ urls.py
@@ -1,10 +1,24 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
+from django.shortcuts import render
 
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+
+def error_page(request, template, status=None):
+    """Render error templates, found in the root /templates directory.
+
+    If no status parameter is explcitedly passed, this function assumes
+    your HTTP status code is the same as your template name (i.e. passing
+    a template=404 will render 404.html with the HTTP status code 404).
+    """
+    return render(request, '%d.html' % template, status=(status or template))
+
+
+handler404 = lambda r: error_page(r, 404)
+handler500 = lambda r: error_page(r, 500)
 
 urlpatterns = patterns('',
     (r'', include('careers.urls')),
@@ -24,4 +38,7 @@ if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
          {'document_root': settings.MEDIA_ROOT}),
+        # Add 404 and 500 pages for testing.
+        (r'^404$', handler404),
+        (r'^500$', handler500),
     )
