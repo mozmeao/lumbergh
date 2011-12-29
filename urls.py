0b5eeab8930ad74ea4e936ee8ab4750da3a18aa5
Jen Fong-Adwent <jen@ednapiranha.com>
2011-12-29 17:56:21 -0500

    removing error call

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import include, patterns, url
+from django.contrib import admin
 from django.shortcuts import render
 
 
@@ -7,14 +8,13 @@ from django.shortcuts import render
 # from django.contrib import admin
 # admin.autodiscover()
 
-def error_page(request, template, status=None):
-    """Render error templates, found in the root /templates directory.
-
-    If no status parameter is explcitedly passed, this function assumes
-    your HTTP status code is the same as your template name (i.e. passing
-    a template=404 will render 404.html with the HTTP status code 404).
-    """
-    return render(request, '%d.html' % template, status=(status or template))
+# def error_page(request, template, status=None):
+#    """Render error templates, found in the root /templates directory.
+#    If no status parameter is explcitedly passed, this function assumes
+#    your HTTP status code is the same as your template name (i.e. passing
+#    a template=404 will render 404.html with the HTTP status code 404).
+#    """
+#    return render(request, '%d.html' % template, status=(status or template))
 
 
 # handler404 = lambda r: error_page(r, 404)
@@ -39,6 +39,6 @@ if settings.DEBUG:
         (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
          {'document_root': settings.MEDIA_ROOT}),
         # Add 404 and 500 pages for testing.
-        (r'^404$', handler404),
-        (r'^500$', handler500),
+        # (r'^404$', handler404),
+        # (r'^500$', handler500),
     )
