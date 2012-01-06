6eff14cd907e9c3236a5ff64fadb8f1576c9f8d1
Jen Fong-Adwent <jen@ednapiranha.com>
2012-01-06 14:17:17 -0500

    added error pages

--- urls.py
+++ urls.py
@@ -8,6 +8,19 @@ from django.shortcuts import render
 # from django.contrib import admin
 # admin.autodiscover()
 
+def error_page(request, template, status=None):
+    """Render error templates, found in the root /templates directory.
+
+    If no status parameter is explcitedly passed, this function assumes
+    your HTTP status code is the same as your template name (i.e. passing
+    a template=404 will render 404.html with the HTTP status code 404).
+    """
+    return render(request, '%d.html' % template, status=(status or template))
+
+
+handler404 = lambda r: error_page(r, 404)
+handler500 = lambda r: error_page(r, 500)
+
 urlpatterns = patterns('',
     (r'', include('careers.urls')),
 
@@ -25,4 +38,6 @@ if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
          {'document_root': settings.MEDIA_ROOT}),
+        (r'^404$', handler404),
+        (r'^500$', handler500),
     )
