0461cefe2f2eae5b37852ae94e7bfe217cc7c5e4
hoosteeno <justin@tentacu.com>
2014-01-31 17:09:04 -0700

    [fix bug 965976] Let back button/links return to filtered listings

--- careers/careers/static/js/position.js
+++ careers/careers/static/js/position.js
@@ -6,6 +6,14 @@
         _gaq.push(['_setCustomVar', 1, 'Team', $('.job-post-team').text(), 3]);
         _gaq.push(['_setCustomVar', 2, 'Location of Job', $('#job-post').data('locationFilter'), 3]);
         _gaq.push(['_trackEvent', 'CV Dummy', 'on load', undefined, undefined, true]);
+
+        // Make back link go back in history (to filtered pages)
+        if (Modernizr.history) {
+            $('.job-back').click(function() {
+                window.history.back();
+                return(false);
+            });
+        }
     });
 
 })(window, window.jQuery);
