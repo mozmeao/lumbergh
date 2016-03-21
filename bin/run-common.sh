422cf2c2542c06c2ca9f5a80f80418bd3021321f
Giorgos Logiotatidis <seadog@sealabs.net>
2016-03-21 14:48:39 +0200

    Add ENABLE_ADMIN switch.

--- bin/run-common.sh
+++ bin/run-common.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python manage.py migrate --noinput
+ENABLE_ADMIN=$(echo "$ENABLE_ADMIN" | tr '[:upper:]' '[:lower:]')
+
+if [[ "$ENABLE_ADMIN" == "true" ]]; then
+    python manage.py migrate --noinput
+fi
