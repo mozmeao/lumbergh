b12411f2e618338b1a3093551bc305628df470c6
Giorgos Logiotatidis <seadog@sealabs.net>
2016-03-21 16:21:42 +0200

    Add EU Prod instance.

--- bin/deploy-travis.sh
+++ bin/deploy-travis.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+DEIS=$1
+APP=$2
+NRAPP=$3
+
 docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker push ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT}
 docker tag -f ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} ${DOCKER_REPOSITORY}:last_successful_build
@@ -8,10 +12,10 @@ docker push ${DOCKER_REPOSITORY}:last_successful_build
 
 # Install deis client
 curl -sSL http://deis.io/deis-cli/install.sh | sh
-./deis login $DEIS_CONTROLLER  --username $DEIS_USERNAME --password $DEIS_PASSWORD
-./deis pull ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} -a $1
+./deis login $DEIS --username $DEIS_USERNAME --password $DEIS_PASSWORD
+./deis pull ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} -a $APP
 curl -H "x-api-key:$NEWRELIC_API_KEY" \
-     -d "deployment[app_name]=$2" \
+     -d "deployment[app_name]=$NRAPP" \
      -d "deployment[revision]=$TRAVIS_COMMIT" \
      -d "deployment[user]=Travis" \
      https://api.newrelic.com/deployments.xml
