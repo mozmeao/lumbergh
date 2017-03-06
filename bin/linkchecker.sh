e4e1540c8272f7d2c38ba345df08d4dd5ef658c5
Giorgos Logiotatidis <seadog@sealabs.net>
2017-03-06 10:54:12 +0200

    Remove deployment from travis.

--- bin/linkchecker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -xe
-HOST=${HOST:-"https://careers.mozilla.org"}
-docker run -v `pwd`:/app python:2 bash -c "cd /app && pip install linkchecker requests==2.9 && linkchecker --check-extern $HOST"
