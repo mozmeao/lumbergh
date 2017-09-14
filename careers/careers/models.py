bb7ca377d39500378b603825ff07aa4859270821
Giorgos Logiotatidis <seadog@sealabs.net>
2017-09-14 11:54:50 +0300

    [fix bug 1398632] Increase maximum size for location field.

--- careers/careers/models.py
+++ careers/careers/models.py
@@ -9,7 +9,7 @@ class Position(models.Model):
     job_id = models.CharField(max_length=10)
     title = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
+    location = models.CharField(max_length=500)
     description = models.TextField()
     apply_url = models.URLField()
     source = models.CharField(max_length=100)
