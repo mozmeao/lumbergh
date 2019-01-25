6f7036abbdf411b97df4d619bbb030e0689b3512
Giorgos Logiotatidis <glogiotatidis@users.noreply.github.com>
2019-01-25 11:04:41 +0200

    Remove university events functionality. (#373)

--- careers/university/tests/test_views.py
+++ careers/university/tests/test_views.py
@@ -1,7 +1,4 @@
-from datetime import date
-
 from django.urls import reverse
-from unittest.mock import patch
 
 from careers.base.tests import TestCase
 from careers.careers.models import Position
@@ -13,37 +10,6 @@ class IndexTests(TestCase):
         url = reverse('university.index')
         response = self.client.get(url, params, follow=True)
         return response, response.context_data
-
-    def test_event_filter(self):
-        """
-        The events shown on the university page should be limited to
-        events occurring on or after the current date, and should be
-        ordered by start date.
-        """
-        event1 = {'name': '1', 'location': 'example',
-                  'start_date': date(2010, 1, 2), 'end_date': date(2010, 1, 6)}
-        event2 = {'name': '2', 'location': 'example',
-                  'start_date': date(2010, 1, 3), 'end_date': date(2010, 1, 8)}
-        event3 = {'name': '3', 'location': 'example',
-                  'start_date': date(2010, 1, 6), 'end_date': date(2010, 1, 6)}
-        event4 = {'name': '4', 'location': 'example',
-                  'start_date': date(2010, 2, 4), 'end_date': date(2010, 2, 4)}
-
-        with patch('careers.university.views.EVENTS', new_callable=list) as events:
-            events.extend([
-                event1, event2, event3, event4,
-                # Events that shouldn't be included in page.
-                {'name': '5', 'location': 'example',
-                 'start_date': date(2010, 1, 2), 'end_date': date(2010, 1, 2)},
-                {'name': '6', 'location': 'example',
-                 'start_date': date(2010, 1, 2), 'end_date': date(2010, 1, 5)}
-            ])
-
-            with patch('careers.university.views.date') as mock_date:
-                mock_date.today.return_value = date(2010, 1, 6)
-                response, context = self._index()
-
-        self.assertEqual(list(context['events']), [event1,  event2, event3, event4])
 
     def test_open_for_applications(self):
         """
