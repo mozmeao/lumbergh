23e60e909691f8236c7f361162ed9d62818aa9b7
Jon Petto <jpetto@users.noreply.github.com>
2019-02-05 16:53:51 -0600

    [fix #340] Add 404 page.

--- careers/urls.py
+++ careers/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import handler404, include, url
 from django.views.static import serve as servestatic
 
 from .base import views
@@ -14,4 +14,7 @@ urlpatterns = [
 
     # Generate a robots.txt
     url(r'^robots\.txt$', views.robots, name='robots'),
+    url(r'^404\.html$', views.custom_404, name='404'),
 ]
+
+handler404 = 'careers.base.views.custom_404'
