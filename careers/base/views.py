a1cbee5e0780a7742c1f0f97bd812429b61d5dc7
Jon Petto <jpetto@users.noreply.github.com>
2019-02-06 08:32:30 -0600

    Fix flake8 errors.

--- careers/base/views.py
+++ careers/base/views.py
@@ -7,6 +7,7 @@ def robots(request):
     action = 'Allow' if settings.ENGAGE_ROBOTS else 'Disallow'
     return HttpResponse('User-agent: *\n{}: /'.format(action), content_type='text/plain')
 
+
 def custom_404(request, exception=None):
     status = 404 if exception else 200
     return render(request, '404.jinja', status=status)
--- careers/urls.py
+++ careers/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import handler404, include, url
+from django.conf.urls import include, url
 from django.views.static import serve as servestatic
 
 from .base import views
