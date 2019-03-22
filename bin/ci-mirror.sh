a1bd7a408ded10ee294d460d12c06fa17925e79d
Dave Parfitt <metadave@protonmail.com>
2019-03-22 16:30:43 -0400

    wip

--- bin/ci-mirror.sh
+++ bin/ci-mirror.sh
@@ -5,7 +5,7 @@ cd ${CI_PROJECT_DIR}
 
 export IMAGE=mozmeao/lumbergh-deploy:`git rev-parse --short HEAD`
 docker build -f ./bin/Dockerfile . -t ${IMAGE}
-docker run -it \
+docker run \
       -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
       -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
       -e BUCKET_PATH="${BUCKET_PATH}" \
