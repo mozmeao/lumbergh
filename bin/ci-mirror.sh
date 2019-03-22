6b9cbaadaf5898877b127ec459fa0d90f77dcf1f
Dave Parfitt <metadave@protonmail.com>
2019-03-22 16:32:45 -0400

    remove error

--- bin/ci-mirror.sh
+++ bin/ci-mirror.sh
@@ -2,7 +2,6 @@
 
 export IMAGE=mozmeao/lumbergh-deploy:`git rev-parse --short HEAD`
 docker build -f ./bin/Dockerfile . -t ${IMAGE}
-asd
 docker run \
       -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
       -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
