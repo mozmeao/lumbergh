88da66ef3ff8c702cbf371c46bc11d090b347430
Dave Parfitt <metadave@protonmail.com>
2019-03-25 11:56:32 -0400

    review cleanup

--- bin/ci-mirror.sh
+++ bin/ci-mirror.sh
@@ -6,8 +6,8 @@
 export IMAGE=mozmeao/lumbergh-deploy:`git rev-parse --short HEAD`
 docker build -f ./bin/Dockerfile . -t ${IMAGE}
 docker run \
-      -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
-      -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
-      -e BUCKET_PATH="${BUCKET_PATH}" \
-      -e DMS="${DMS}" \
+      -e AWS_ACCESS_KEY_ID \
+      -e AWS_SECRET_ACCESS_KEY \
+      -e BUCKET_PATH \
+      -e DMS \
       ${IMAGE} 
