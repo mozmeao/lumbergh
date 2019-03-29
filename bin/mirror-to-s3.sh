4bf20c35c06451c1fedff05ce3758fb657e1dd4e
Dave Parfitt <metadave@protonmail.com>
2019-03-29 15:36:29 -0400

    set cache-control headers on push

---
+++ bin/mirror-to-s3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+echo $(PWD)
+./bin/mirror.sh 
+
+aws s3 sync ./_site ${BUCKET_PATH} \
+    --acl public-read \
+    --cache-control "max-age=315360000, public, immutable"  \
+    --delete
+
+aws s3 cp ./_site/listings/index.html ${BUCKET_PATH}/listings/index.html \
+    --acl public-read \
+    --cache-control "max-age=1800, public" \
+
+curl ${DMS}
+
