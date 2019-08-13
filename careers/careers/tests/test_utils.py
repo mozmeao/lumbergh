71347930699e5564c969c1673ce5cb0581d74c58
Jonathan Petto <jpetto@users.noreply.github.com>
2019-08-13 16:18:24 -0500

    [fix #451] Add META descriptions.
    - Adds util function to generate META for single listing page

---
+++ careers/careers/tests/test_utils.py
@@ -0,0 +1,41 @@
+from careers.base.tests import TestCase
+from careers.careers.tests import PositionFactory
+from careers.careers.utils import generate_position_meta_description
+
+
+class GeneratePositionMetaDescriptionTests(TestCase):
+    def setUp(self):
+        self.position = PositionFactory.create(
+            title='Bowler',
+            position_type='Full time',
+            location='Los Angeles,Ralphs')
+
+    def test_position_type_consonant_beginning(self):
+        meta = generate_position_meta_description(self.position)
+        self.assertEqual(meta, 'Mozilla is hiring a full time Bowler in Los Angeles and Ralphs')
+
+    def test_position_type_vowel_beginning(self):
+        self.position.position_type = 'intern'
+        meta = generate_position_meta_description(self.position)
+        self.assertEqual(meta, 'Mozilla is hiring an intern Bowler in Los Angeles and Ralphs')
+
+    def test_single_location(self):
+        self.position.location = 'Los Angeles'
+        meta = generate_position_meta_description(self.position)
+        self.assertEqual(meta, 'Mozilla is hiring a full time Bowler in Los Angeles')
+
+    def test_multiple_locations(self):
+        self.position.location = 'Los Angeles,Ralphs,In-N-Out'
+        meta = generate_position_meta_description(self.position)
+        self.assertEqual(
+            meta, 'Mozilla is hiring a full time Bowler in In-N-Out, Los Angeles and Ralphs')
+
+    def test_truncation(self):
+        self.position.title = ('Senior Executive Director of Bowling Data'
+                               ' Infrastructure Management - Europe')
+        self.position.location = 'Los Angeles,Ralphs,In-N-Out,Malibu,La Jolla'
+        meta = generate_position_meta_description(self.position)
+        self.assertEqual(
+            meta, ('Mozilla is hiring a full time Senior Executive Director of Bowling Data '
+                   'Infrastructure Management - Europe in In-N-Out, La Jolla, '
+                   'Los Angeles, Malibu and Ral&hellip;'))
---
+++ careers/careers/utils.py
@@ -0,0 +1,17 @@
+def generate_position_meta_description(position):
+    suffix = 'n' if position.position_type.lower().startswith(('a', 'e', 'i', 'o', 'u')) else ''
+    meta = (
+        f'Mozilla is hiring a{suffix} '
+        f'{position.position_type.lower()} {position.title} in '
+    )
+
+    if len(position.location_list) > 1:
+        meta = meta + f'{", ".join(position.location_list[:-1])} and {position.location_list[-1]}'
+    else:
+        meta = meta + position.location_list[0]
+
+    # finally, truncate if over 160 characters
+    if len(meta) >= 160:
+        meta = meta[:157] + '&hellip;'
+
+    return meta
