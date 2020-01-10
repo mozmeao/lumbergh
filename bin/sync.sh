d481ef3fdb523e0c9c1a3b2c299b358fa6f4cce8
Alan Alexander <aalexander@mozilla.com>
2020-01-10 11:41:02 -0800

    adding some timing info to the sync script

--- bin/sync.sh
+++ bin/sync.sh
@@ -3,6 +3,8 @@
 # `-f` escapes `*`
 set -xf
 
+echo "Before Syncs"
+echo $(date +%s)
 
 # Sync static assets first, before uploading other files that reference them.
 # Set long live cache headers.
@@ -13,9 +15,15 @@ aws s3 sync ./_site ${BUCKET_PATH} \
     --acl public-read \
     --delete
 
+echo "After Static Sync, before Non-static Sync"
+echo $(date +%s)
+
 # Sync the rest of the files.
 aws s3 sync ./_site ${BUCKET_PATH} \
     --include "*" \
     $(jq .paths[] static/staticfiles.json | xargs -I {} echo -n "--exclude static/{}* ") \
     --acl public-read \
     --delete
+
+echo "After Static Syncs"
+echo $(date +%s)
