9b3b7c614b751e6a206ff3b736b9e951d558cc23
Giorgos Logiotatidis <seadog@sealabs.net>
2020-01-13 15:08:43 +0200

    correct path.

--- bin/sync.sh
+++ bin/sync.sh
@@ -9,7 +9,7 @@ BUCKET_NAME=${BUCKET_PATH:5}
 # Set long live cache headers using `aws s3api` only to files that changed.
 ./bin/rclone --config ./bin/rclone.conf --no-update-modtime -v \
        $(jq .paths[] static/staticfiles.json | xargs -I {} echo -n "--include static/{} ") \
-       sync . ${BUCKET_PATH} 2>&1 | grep "Copied" | cut -d ":" -f 4 | xargs -I '{}' aws s3api copy-object \
+       sync ./_site ${BUCKET_PATH} 2>&1 | grep "Copied" | cut -d ":" -f 4 | xargs -I '{}' aws s3api copy-object \
                                                                         --acl public-read \
                                                                         --copy-source ${BUCKET_NAME}/'{}' \
                                                                         --bucket  ${BUCKET_NAME} \
@@ -21,4 +21,4 @@ BUCKET_NAME=${BUCKET_PATH:5}
 # Sync the rest of the files.
 ./bin/rclone --config ./bin/rclone.conf --no-update-modtime \
        $(jq .paths[] static/staticfiles.json | xargs -I {} echo -n "--exclude static/{} ") \
-       sync . ${BUCKET_PATH}
+       sync ./_site ${BUCKET_PATH}
