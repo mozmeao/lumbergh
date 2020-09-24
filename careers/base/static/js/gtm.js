f225fa8220c12c844753c8c0c50aa2d544829e88
Paul McLanahan <pmac@mozilla.com>
2020-09-24 09:21:17 -0400

    Update GA and GTM files to bedrock standards
    Use the DNT helper to only load GA or GTM when the user has not
    set DNT. Also move some code around to try to comply with the GTM
    recommendations.

--- careers/base/static/js/gtm.js
+++
@@ -1,7 +0,0 @@
-var _gtmAccountCode = document.documentElement.getAttribute('data-gtm-code');
-
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer', _gtmAccountCode);
